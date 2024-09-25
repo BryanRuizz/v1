ceb60e3947283f8c73677e15343afa34d75b696a
bryan ruiz <bryanruiz2468@gmail.com>
2024-09-24 19:06:03 -0600

    Refactor: delete/public
    just /directory

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,8 +5,8 @@ export const dataprojects = [
         name: "Information Manager erp",
         label: "Erp system",
         desc: "Enterprise resource management system, non-commercial, modular, comprehensive and customised.",
-        srcsett: "/public/principalimg/aldafaerp.webp 1x, /public/principalimg/aldafaerp.webp 2x",
-        mainimg: "/public/principalimg/aldafaerp.webp",
+        srcsett: "/principalimg/aldafaerp.webp 1x, /principalimg/aldafaerp.webp 2x",
+        mainimg: "/principalimg/aldafaerp.webp",
 
     },
     {
@@ -14,15 +14,15 @@ export const dataprojects = [
         name: "Aldafa web page",
         label: "Erp system",
         desc: "Main presentation web page to publicise the personal or business brand and attract sales.",
-        srcsett: "/public/principalimg/aldafamain.webp 1x, /public/principalimg/aldafamain.webp 2x",
-        mainimg: "/public/principalimg/aldafamain.webp",
+        srcsett: "/principalimg/aldafamain.webp 1x, /principalimg/aldafamain.webp 2x",
+        mainimg: "/principalimg/aldafamain.webp",
     },
     {
         href: "",
         name: "Savvy bucks",
         label: "Loans and savings for business",
         desc: "A user-friendly mobile application designed to manage both savings and business loans, tailored for the general public, with an intuitive interface",
-        srcsett: "/public/principalimg/savvy.webp 1x, /public/principalimg/savvy.webp 2x",
-        mainimg: "/public/principalimg/savvy.webp",
+        srcsett: "/principalimg/savvy.webp 1x, /principalimg/savvy.webp 2x",
+        mainimg: "/principalimg/savvy.webp",
     },
 ]
